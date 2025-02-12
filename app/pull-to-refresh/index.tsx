d35e16208ad8efbc915bdf40e3a9a7eeaae431e8
hecmango <hamancia@innovacion.gob.sv>
2025-02-12 09:33:41 -0600

    pull to refresh

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import ThemedView from '@/presentation/shared/ThemedView';
+import ThemeText from '@/presentation/shared/ThemeText';
+import { useState } from 'react';
+import { View, Text, ScrollView, RefreshControl } from 'react-native';
 
 const PullToRefreshScreen = () => {
+
+  const primaryColor = useThemeColor({}, 'primary')
+  const backgroundColor = useThemeColor({
+    dark:'yellow',
+    light: 'yellow'
+  }, 'background')
+
+  const onRefresh = () => {
+    setisRefreshing(true)
+    setTimeout(() => {
+      setisRefreshing(false)
+    }, 3000)
+  }
+
+  const [isRefreshing, setisRefreshing] = useState(false)
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+    refreshControl={
+      <RefreshControl
+      refreshing={isRefreshing}
+      onRefresh={onRefresh}
+      colors={[primaryColor, 'red', 'blue']}
+      progressBackgroundColor={backgroundColor}
+      />
+    }
+    >
+      <ThemedView margin>
+        <ThemeText>Pull to refresh</ThemeText>
+      </ThemedView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
